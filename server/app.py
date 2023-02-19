a41821ca5ea7ca52c8bb1ceac8f2b9db8194f04e
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-02-19 01:58:16 -0800

    added endpoints

--- server/app.py
+++ server/app.py
@@ -1,11 +1,83 @@
-from flask import Flask
+from vosk import Model, KaldiRecognizer
+import pyaudio
+from utils.sentimentAnalysis import getPerspectiveScore, getGPTScore
+from utils.webcam import webcamController
+import threading
+from flask import Flask, jsonify, Response
 
-import utils.speechToText, utils.sentimentAnalysis, utils.speechToText
+
+model = Model(model_name = "vosk-model-small-en-us-0.15")
+
+score_history = [0, 0, 0]   # tracks last 5 responses
+response_history = ['', '', '']
+
+recognizer = KaldiRecognizer(model, 16000)
+
+mic = pyaudio.PyAudio()
+stream = mic.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8192)
+
+                    
+webcamcontroller = webcamController()
+webcam_thread = threading.Thread(target=webcamcontroller.run)
+webcam_thread.start()
+
+
 app = Flask(__name__)
 
-@app.route('/')
-def intro():
-    return utils.speechToText.getStreamStatus()
+def runWebServer():
+    app.run()
 
-if __name__ == "__main__":
-    app.run(debug=True)
+def speechMonitoring():
+    while True:
+        data = stream.read(4096, exception_on_overflow=False)
+        if recognizer.AcceptWaveform(data):
+            text = recognizer.Result()
+            text = text[14:-3]
+            if text != '':
+                print(text)
+                response_history.pop(0)
+                response_history.append(text)
+                score_history.pop(0)
+                score_history.append(getPerspectiveScore(text))
+                print(score_history)
+                if (sum(score_history)/len(score_history) > 0.5 and webcamcontroller.streaming == False):
+                    print("***************\nSTARTING WEBCAM\n***************")
+                    webcamcontroller.streaming = True
+                elif (webcamcontroller.streaming == True):
+                    print("***************\STOPPING WEBCAM\n***************")
+                    webcamcontroller.streaming = False
+
+@app.route('/')
+def sendStreamStatus():
+    if webcamcontroller.streaming:
+        return '<h1>streaming</h1>'
+    else:
+        return '<h1>not streaming</h1>'
+
+@app.route('/transcript')
+def sendTranscript():
+    return jsonify({
+        '0': response_history[0],
+        '1': response_history[1],
+        '2': response_history[2]
+    })
+
+
+@app.route("/stream")
+def sendWebcam():
+    if webcamcontroller.streaming:
+        return Response(webcamcontroller.run(), mimetype = "multipart/x-mixed-replace; boundary=frame")
+    else:
+        return "<h1>No stream data</h1>"
+    
+
+
+
+if __name__ == '__main__':
+    try:
+        serverThread = threading.Thread(target=runWebServer).start()
+        monitoringThread = threading.Thread(target=speechMonitoring).start()
+    except Exception as e:
+        print(str(e))
+
+    
--- server/utils/webcam.py
+++ server/utils/webcam.py
@@ -1,32 +1,40 @@
 import cv2
+import threading
+
+
+lock = threading.Lock()
 
 class webcamController:
 
     def __init__(self) -> None:
         self.streaming = False
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
     def stop(self):
         self.streaming = False
-        cv2.destroyAllWindows()
         
     def start(self):
         self.streaming = True
 
-    def run(self, cap):
-        while True:
-            while self.streaming:
-                try:
-                    
-                    ret, frame = cap.read()
+    def run(self):
+        while self.streaming:
+            try:
+                with lock:
+                    ret, frame = self.cap.read()
+                    (flag, encodedImage) = cv2.imencode(".jpg", frame)  
+                    if not flag:
+                        continue 
+                yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
+                exitKey = cv2.waitKey(10) & 0xFF
+                if exitKey == 27:
+                    break
 
-                    cv2.imshow("Dashcam Footage", frame)
+            except cv2.error as e:
+                print(str(e))
 
-                    exitKey = cv2.waitKey(10) & 0xFF
-                    if exitKey == 27:
-                        break
-
-                except cv2.error as e:
-                    print(str(e))
+        self.cap.release()
 
 
 
